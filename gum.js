08cde7c669c21c9bf07dd148deed5d2f70d94500
Johannes Odland <johannes.odland@teliasonera.com>
2013-10-26 11:50:34 +0200

    Video from gUM

--- gum.js
+++ gum.js
@@ -4,3 +4,21 @@ navigator.getMedia = ( navigator.getUserMedia ||
         navigator.msGetUserMedia);
 
 
+navigator.getMedia(
+        // constraints
+        {
+            video: true,
+            audio: false
+        },
+        // successCallback
+        function(stream) {
+            var video = document.querySelector('#icv');
+            video.src = (window.URL && window.URL.createObjectURL( stream )) || stream;
+            video.play();
+        },
+
+        // errorCallback
+        function(err) {
+            console.log("The following error occured: " + err);
+        }
+);
